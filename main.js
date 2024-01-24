3393a57dac3ae16c8c24b27b988521053adf9860
tubagus.fakhridhiya@gmail.com <tubagus.fakhridhiya@gmail.com>
2024-01-24 10:48:50 +0700

    update main.js

--- main.js
+++ main.js
@@ -12,12 +12,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDE9RCp2gUIyOG8hUEv5un0JlXqHWowkA",
+apiKey: "AIzaSyCDE9RCp2gUIyOG8hUEv5un0JlXqHWowkA",
   authDomain: "crud-6df8e.firebaseapp.com",
   projectId: "crud-6df8e",
   storageBucket: "crud-6df8e.appspot.com",
   messagingSenderId: "22846946310",
   appId: "1:22846946310:web:f393dbd1ff1ad9d61f1787"
 };
+
 
 const app = initializeApp(firebaseConfig);
